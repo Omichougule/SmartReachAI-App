5db8b9c2d595e495dcb01340dd4cb9d7822f6d2c
Omkar Chougule <63452279+Omichougule@users.noreply.github.com>
2025-05-01 23:31:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
 
-
+import os
+os.environ["CHROMA_USE_PYSQLITE3"] = "1"
 def create_streamlit_app(llm, portfolio, clean_text):
     st.markdown("""
     <style>
