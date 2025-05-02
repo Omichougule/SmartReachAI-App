3d674b7a772e40c111ef095701e157623f99e4ea
Omkar Chougule <63452279+Omichougule@users.noreply.github.com>
2025-05-01 23:42:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+os.environ["CHROMA_USE_PYSQLITE3"] = "1"
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
@@ -5,8 +7,6 @@ from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
 
-import os
-os.environ["CHROMA_USE_PYSQLITE3"] = "1"
 def create_streamlit_app(llm, portfolio, clean_text):
     st.markdown("""
     <style>
