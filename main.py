eef7f2a9db2b29be9ab72b3eb872bd886b108908
Omkar Chougule <63452279+Omichougule@users.noreply.github.com>
2025-05-02 22:59:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-import os
-os.environ["CHROMA_USE_PYSQLITE3"] = "1"
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
+
+
 
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
+
 
 def create_streamlit_app(llm, portfolio, clean_text):
     st.markdown("""
